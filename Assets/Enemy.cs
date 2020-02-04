6013a9ec15f4e2f24f6adcdc64316dab7bb0587a
greenfieldxd <monsterbig53@gmail.com>
2020-02-04 20:18:09 +0300

    Game Objects Test

---
+++ Assets/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public string enemyName;
+    public Player playerVar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, " + playerVar.playerName + ", I am is your Enemy");
+        Debug.Log(playerVar);
+    }
+
+    public void RunAway()
+    {
+        Debug.Log("I'm scared");
+    }
+
+    void Win()
+    {
+        Debug.Log("I WON!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string playerName;
+    public Enemy enemyVar;
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, my name is " + playerName);
+        Debug.Log(enemyVar);
+
+        enemyVar.RunAway();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
