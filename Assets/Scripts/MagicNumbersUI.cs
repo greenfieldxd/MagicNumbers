328e40ca352e607fe951a0babf45455778d04ddf
greenfieldxd <monsterbig53@gmail.com>
2020-02-13 21:17:55 +0300

    Create MagicNumbersUI

---
+++ Assets/Scripts/MagicNumbersUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MagicNumbersUI : MonoBehaviour
+{
+
+    public int minNumber = 1;
+    public int maxNumber = 1000;
+    int guess;
+    int attempts = 1;
+
+    public Text textGuess;
+    public TextMeshProUGUI textCount;
+
+
+    void Start()
+    {
+        //guess = (minNumber + maxNumber) / 2;
+        guess = Random.Range(minNumber, maxNumber + 1);
+        textGuess.text = "Ваше число " + guess + "?";
+        textCount.text = "Колличество ходов: " + attempts;
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        int activeSneneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(activeSneneIndex + 1);
+    }
+
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
