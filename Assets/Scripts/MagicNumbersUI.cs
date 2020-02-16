ed0961179dbafb40f674fa483d0a52f51dfa8287
greenfieldxd <monsterbig53@gmail.com>
2020-02-16 19:06:47 +0300

    Compled game flow (UI)

--- Assets/Scripts/MagicNumbersUI.cs
+++ Assets/Scripts/MagicNumbersUI.cs
@@ -18,16 +18,31 @@ public class MagicNumbersUI : MonoBehaviour
 
     void Start()
     {
-        //guess = (minNumber + maxNumber) / 2;
+        //guess = (minNumber + maxNumber) / 2;   до этого работало через эту строку, сделали через Рандом
+
         guess = Random.Range(minNumber, maxNumber + 1);
         textGuess.text = "Ваше число " + guess + "?";
-        textCount.text = "Колличество ходов: " + attempts;
+        textCount.text = "Attempts: " + attempts;
 
     }
 
-    
-    void Update()
+    public void Lager()
     {
-        
+        minNumber = guess;
+        UpdateGuess();
+        Start();
+    }
+
+    public void Smaller()
+    {
+        maxNumber = guess;     
+        UpdateGuess();
+        Start();
+    }
+
+    void UpdateGuess()
+    {
+        guess = (minNumber + maxNumber) / 2;
+        attempts++;
     }
 }
