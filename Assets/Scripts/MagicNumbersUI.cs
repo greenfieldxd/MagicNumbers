52b41a70fddc058fe79e70f2a2b3e231abca1781
greenfieldxd <monsterbig53@gmail.com>
2020-02-18 21:11:28 +0300

    changes on lesson

--- Assets/Scripts/MagicNumbersUI.cs
+++ Assets/Scripts/MagicNumbersUI.cs
@@ -9,8 +9,9 @@ public class MagicNumbersUI : MonoBehaviour
 
     public int minNumber = 1;
     public int maxNumber = 1000;
-    int guess;
+    
     int attempts = 1;
+    public int guess;
 
     public Text textGuess;
     public TextMeshProUGUI textCount;
@@ -18,8 +19,12 @@ public class MagicNumbersUI : MonoBehaviour
 
     void Start()
     {
-        //guess = (minNumber + maxNumber) / 2;   до этого работало через эту строку, сделали через Рандом
+        
 
+        DontDestroyOnLoad(gameObject);
+
+        //guess = (minNumber + maxNumber) / 2;   до этого работало через эту строку, сделали через Рандом
+        
         guess = Random.Range(minNumber, maxNumber + 1);
         textGuess.text = "Ваше число " + guess + "?";
         textCount.text = "Attempts: " + attempts;
---
+++ Assets/Scripts/ResultScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScene : MonoBehaviour
+{
+
+    public Text resultText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MagicNumbersUI magicNumbers = FindObjectOfType<MagicNumbersUI>();
+        int number = magicNumbers.guess;
+        resultText.text = "Ты молодец! Компьютеру было сложно, но он все равно смог отгадать твое число, равное " + number + ".";
+
+        Destroy(magicNumbers.gameObject); //Destroy Game Object 
+        //Destoy(magicNumbers); //destroy component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
